14c9c9f8dd1f3154fa75d138cdf5c6fd5a3c6abe
Jason Brunch <jasonbunce2@gmail.com>
2023-08-31 16:58:54 -0700

    impomented bootstrap nav bar and side bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React, {useState} from 'react';
+import NavBar from './NavBar';
+import SideMenu from './SideMenu';
+import NoteEditor from './NoteEditor';
+
+function App(){
+  const sampleNotes = [
+    { id: 1, title: "Note 1", content: "Content 1" },
+    { id: 2, title: "Note 2", content: "Content 2" }
+  ];
+    const [notes, setNotes] = useState(sampleNotes);
+
+  return(
+    <div className='App'>
+      <NavBar />
+      <div className='container-fluide'>
+        <div className="row">
+          <div className="col-lg-3 col-md-f col-dm-12">
+            <SideMenu />
+          </div>
+          <div className='col-log-9 col-md-8 col-sm-12'>
+            <NoteEditor />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
 export default App;
